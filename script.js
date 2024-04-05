18b8ba5d7074b859ca3c631b2d2dce7433ea6459
Wellington <welltecnic@hotmail.com>
2024-04-05 09:28:29 -0300

    criando o script

---
+++ script.js
@@ -0,0 +1,7 @@
+function Calcular(){
+    //DECLARAR AS VARIAVEIS
+ let num1 = parseFloat(document.getElementById('num1').value);
+ let num2 = parseFloat(document.getElementById('num2').value);
+ document.getElementById('resultado').innerHTML =num1 + num2;
+
+}
